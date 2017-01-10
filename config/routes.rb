bf5c694d8d529580e6abdfda62c71fe413bc1d78
Michael Welsh <brohemian1989@gmail.com>
2017-01-10 17:34:39 -0500

    added static pages enabled user to end their session, Closes #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+      
+      get '/' => 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
