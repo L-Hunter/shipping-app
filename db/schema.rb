c986e0296a774812c3f95ac25f2db33d4116ca13
TheButlerDidIt <labutler@gmail.com>
2017-01-12 15:36:58 -0500

    completed boat and job forms, adds name column to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111192152) do
+ActiveRecord::Schema.define(version: 20170112200642) do
 
   create_table "boats", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170111192152) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
