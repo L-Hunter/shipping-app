f22b21ca80b354f5332baa7a1c7cbccb6d4e468e
Michael Welsh <brohemian1989@gmail.com>
2017-01-13 16:13:34 -0500

    added more styling for profiles page.

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
       }
       
       get '/' => 'static_pages#home'
+
+      get '/about' => 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
