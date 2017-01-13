5a65d1337d194a244ce139ba7ff8d6b9315f6094
Ryan Courtney <ryan.courtney1@gmail.com>
2017-01-13 11:45:13 -0500

    needs merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110204709) do
-
 ActiveRecord::Schema.define(version: 20170112200642) do
-
 
   create_table "boats", force: :cascade do |t|
     t.string   "name"
